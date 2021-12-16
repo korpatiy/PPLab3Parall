73f7441848c062404a2703d59432857ea4f0cc87
Vyacheslav Tretyakov <tretiakovslava@gmail.com>
2021-12-17 00:11:17 +0500

    worked version

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-const int N = 11;
+const int N = 111;
 const double a = 1.0;
 const double h = a / N;
 const double tau = h * h / 2 / 2;
@@ -18,166 +18,192 @@ const double L2 = pow(L, 2);
 
 //функция по варианту
 double func(double x) {
-  if (x <= 0.7 && x >= 0.5)
-    return (9.0 * x - 4.5) / 0.2;
-  if (x > 0.7 && x <= 0.9)
-    return (-9.0 * x + 8.1) / 0.2;
-  return 0.0;
+    if (x <= 0.7 && x >= 0.5)
+        return (9.0 * x - 4.5) / 0.2;
+    if (x > 0.7 && x <= 0.9)
+        return (-9.0 * x + 8.1) / 0.2;
+    return 0.0;
 }
 
 double g(double x) { return 0.0; }
 
-//функция по методичке
-double funcExample(double x) {
-  if (x < 0.5) return 2.0 * x;
-  return 2.0 - 2.0 * x;
-}
-
 double countRightPrev(int currN, int currSize, const double *currU, double right_prev, int t) {
-  return 2.0 * (1.0 - L2) * currU[t * currSize + currN]
-      + L2 * (currU[t * currSize + currN - 1] + right_prev)
-      - currU[(t - 1) * currSize + currN];
+    return 2.0 * (1.0 - L2) * currU[t * currSize + currN]
+           + L2 * (currU[t * currSize + currN - 1] + right_prev)
+           - currU[(t - 1) * currSize + currN];
 }
 
 double countLeftPrev(int currSize, const double *currU, double left_prev, int t) {
-  return 2.0 * (1.0 - L2) * currU[t * currSize]
-      + L2 * (left_prev + currU[t * currSize + 1])
-      - currU[(t - 1) * currSize];
+    return 2.0 * (1.0 - L2) * currU[t * currSize]
+           + L2 * (left_prev + currU[t * currSize + 1])
+           - currU[(t - 1) * currSize];
 }
 
 //Умф по методичке
 double umf(int currSize, const double *currU, int t, int i) {
-  return 2.0 * (1.0 - L2) * currU[t * currSize + i] + L2 * (currU[t * currSize + i + 1] + currU[t * currSize + i - 1])
-      - currU[(t - 1) * currSize + i];
+    return 2.0 * (1.0 - L2) * currU[t * currSize + i] + L2 * (currU[t * currSize + i + 1] + currU[t * currSize + i - 1])
+           - currU[(t - 1) * currSize + i];
 }
 
 int main(int argc, char **argv) {
 
-  int rank, size;
-  MPI_Status status;
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  double time = MPI_Wtime();
+    if ((N + 1) % size != 0) {
+        std::cout << N + 1 << " должно делиться на число процессов без остатка";
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
 
-  int dims[1] = {0};
-  MPI_Dims_create(size, 1, dims);
-  int periods[1] = {0};
+    auto time1 = MPI_Wtime();
 
-  MPI_Comm comm_1D;
-  MPI_Cart_create(MPI_COMM_WORLD, 1, dims, periods, true, &comm_1D);
+    int dims[1] = {0};
+    MPI_Dims_create(size, 1, dims);
+    int periods[1] = {false};
 
-  //Координаты соседей
-  int left;
-  int right;
-  //Нахождение соседей слева и справа
-  MPI_Cart_shift(comm_1D, 0, 1, &left, &right);
+    MPI_Comm comm_1D;
+    MPI_Cart_create(MPI_COMM_WORLD, 1, dims, periods, true, &comm_1D);
 
-  MPI_Comm_rank(comm_1D, &rank);
+    //Координаты соседей
+    int left;
+    int right;
+    //Нахождение соседей слева и справа
+    MPI_Cart_shift(comm_1D, 0, 1, &left, &right);
 
-  //Нарезаем часть, которую будет обрабатывать каждый процессор
-  auto matrixPart = (Nt + 1) / size;
+    MPI_Comm_rank(comm_1D, &rank);
 
-  //Произвольный тип данных для строки
-  MPI_Datatype MPI_RAW;
-  MPI_Type_vector(1, matrixPart, matrixPart, MPI_DOUBLE, &MPI_RAW);
-  MPI_Type_commit(&MPI_RAW);
+    //Нарезаем часть, которую будет обрабатывать каждый процессор
+    auto matrixPart = (N + 1) / size;
 
-  auto begIdx = matrixPart * rank;
-  auto endIdx = (rank == size - 1 ? N : begIdx + matrixPart - 1);
+    //Произвольный тип данных для строки
+    MPI_Datatype MPI_RAW;
+    MPI_Type_vector(1, matrixPart, matrixPart, MPI_DOUBLE, &MPI_RAW);
+    MPI_Type_commit(&MPI_RAW);
 
-  auto currN = endIdx - begIdx;
-  auto currSize = currN + 1;
-  auto *currU = new double[(Nt + 1) * currSize];
+    auto begIdx = matrixPart * rank;
+    auto endIdx = (rank == size - 1 ? N : begIdx + matrixPart - 1);
 
-  //начальные и краевые условия
-  if (begIdx == 0)
-    currU[currSize] = currU[0] = 0.0;
+    auto currN = endIdx - begIdx;
+    auto currSize = currN + 1;
+    auto *currU = new double[(Nt + 1) * currSize];
 
-  if (endIdx == N)
-    currU[currSize + currN] = currU[currN] = 0.0;
-
-  for (int i = 1; i <= currN; i++) {
-    //x от 0 до 1 в i * h
-    currU[i] = func((begIdx + i) * h); //y1
-    currU[currSize + i] = currU[i] + tau * g(begIdx + i); //y2
-  }
-
-  double left_prev, right_prev;
-  MPI_Send(&currU[1 * currSize + 0], 1, MPI_DOUBLE, left, 0, comm_1D);
-  MPI_Send(&currU[1 * currSize + currN], 1, MPI_DOUBLE, right, 0, comm_1D);
-  MPI_Recv(&left_prev, 1, MPI_DOUBLE, left, 0, comm_1D, &status);
-  MPI_Recv(&right_prev, 1, MPI_DOUBLE, right, 0, comm_1D, &status);
-
-  //Идем по времени от 0 до T.
-  for (int t = 1; t < Nt; t++) {
+    //начальные и краевые условия
+    int k = 0;
+    int end = currN;
     if (begIdx == 0) {
-      currU[(t + 1) * currSize] = 0.0;
-      if (endIdx == N)
-        currU[(t + 1) * currSize + currN] = 0.0;
-      else
-        currU[(t + 1) * currSize + currN] = countRightPrev(currN, currSize, currU, right_prev, t);
-    } else {
-      if (endIdx == N) {
-        currU[(t + 1) * currSize + currN] = 0.0;
-        currU[(t + 1) * currSize] = countLeftPrev(currSize, currU, left_prev, t);
-      } else {
-        if (currN == 0)
-          currU[(t + 1) * currSize] = 2.0 * (1.0 - L2) * currU[t * currSize]
-              + L2 * (left_prev + right_prev)
-              - currU[(t - 1) * currSize];
-        else {
-          currU[(t + 1) * currSize + currN] = countRightPrev(currN, currSize, currU, right_prev, t);
-          currU[(t + 1) * currSize] = countLeftPrev(currSize, currU, left_prev, t);
+        currU[currSize] = currU[0] = 0.0;
+        k++;
+    }
+
+    if (endIdx == N) {
+        currU[currSize + currN] = currU[currN] = 0.0;
+        end--;
+    }
+
+    for (; k <= end; k++) {
+        //x от 0 до 1 в k * h
+        currU[k] = func((begIdx + k) * h); //y1
+        currU[currSize + k] = currU[k] + tau * g(begIdx + k); //y2
+    }
+
+    MPI_Status status1, status2;
+
+    double left_prev, right_prev;
+    MPI_Send(&currU[currSize], 1, MPI_DOUBLE, left, 0, comm_1D);
+    MPI_Send(&currU[currSize + currN], 1, MPI_DOUBLE, right, 0, comm_1D);
+    MPI_Recv(&left_prev, 1, MPI_DOUBLE, left, 0, comm_1D, &status1);
+    MPI_Recv(&right_prev, 1, MPI_DOUBLE, right, 0, comm_1D, &status2);
+
+    //Идем по времени от 0 до T.
+    for (int t = 1; t < Nt; t++) {
+        auto currTIdx = (t + 1) * currSize;
+        if (begIdx == 0) {
+            currU[currTIdx] = 0.0;
+            if (endIdx == N)
+                currU[currTIdx + currN] = 0.0;
+            else
+                currU[currTIdx + currN] = countRightPrev(currN, currSize, currU, right_prev, t);
+        } else {
+            if (endIdx == N) {
+                currU[currTIdx + currN] = 0.0;
+                currU[currTIdx] = countLeftPrev(currSize, currU, left_prev, t);
+            } else {
+                if (currN == 0)
+                    currU[currTIdx] = 2.0 * (1.0 - L2) * currU[t * currSize]
+                                      + L2 * (left_prev + right_prev)
+                                      - currU[(t - 1) * currSize];
+                else {
+                    currU[currTIdx + currN] = countRightPrev(currN, currSize, currU, right_prev, t);
+                    currU[currTIdx] = countLeftPrev(currSize, currU, left_prev, t);
+                }
+            }
         }
-      }
+
+        for (int i = 1; i < currN; i++) {
+            currU[currTIdx + i] = umf(currSize, currU, t, i);
+        }
+
+        MPI_Send(&currU[currTIdx], 1, MPI_DOUBLE, left, 0, comm_1D);
+        MPI_Send(&currU[currTIdx + currN], 1, MPI_DOUBLE, right, 0, comm_1D);
+
+        MPI_Recv(&left_prev, 1, MPI_DOUBLE, left, 0, comm_1D, &status1);
+        MPI_Recv(&right_prev, 1, MPI_DOUBLE, right, 0, comm_1D, &status2);
     }
 
-    for (int i = 1; i < currN; i++) {
-      currU[(t + 1) * currSize + i] = umf(currSize, currU, t, i);
+
+    double *fullTime = nullptr;
+    double *uFull = nullptr;
+    if (rank == 0) {
+        uFull = new double[(Nt + 1) * size * matrixPart];
+        fullTime = new double[size];
     }
 
-    MPI_Send(&currU[(t + 1) * currSize], 1, MPI_DOUBLE, left, 0, comm_1D);
-    MPI_Send(&currU[(t + 1) * currSize + currN], 1, MPI_DOUBLE, right, 0, comm_1D);
+    //Собираем кусочки
+    MPI_Gather(&currU[0], Nt + 1, MPI_RAW,
+               &uFull[0], Nt + 1, MPI_RAW, 0, comm_1D);
 
-    MPI_Recv(&left_prev, 1, MPI_DOUBLE, left, 0, comm_1D, &status);
-    MPI_Recv(&right_prev, 1, MPI_DOUBLE, right, 0, comm_1D, &status);
-  }
+    auto time2 = MPI_Wtime() - time1;
+    //Собираем время
+    MPI_Gather(&time2, 1, MPI_DOUBLE,
+               fullTime, 1, MPI_DOUBLE, 0, comm_1D);
 
-  auto *uFull = new double[(Nt + 1) * size * matrixPart];
+    if (rank == 0) {
 
-  //Собираем
-  MPI_Gather(&currU[0], Nt + 1, MPI_RAW,
-             &uFull[0], Nt + 1, MPI_RAW, 0, comm_1D);
+        ofstream fileOutput = ofstream("output.txt");
+        auto maxTime = fullTime[0];
+        for (int i = 0; i < size; i++) {
+            if (fullTime[i] > maxTime)
+                maxTime = fullTime[i];
+        }
+        fileOutput << "Time: " << maxTime * 1000 << "\n";
 
-  if (rank == 0) {
+        //выводим координаты x
+        fileOutput << 0 << ",";
+        for (int i = 0; i < N; i++) {
+            fileOutput << i * h << ",";
+        }
+        fileOutput << 1 << "\n";
 
-    ofstream fileOutput = ofstream("output.txt");
-    //fileOutput << "Time: " << max_time * 1000 << "\n";
-
-    //выводим координаты x
-    fileOutput << 0 << ",";
-    for (int i = 0; i < N; i++) {
-      fileOutput << i * h << ",";
+        for (int i = 0; i < Nt + 1; i++) {
+            //координаты по T
+            fileOutput << i * tau << ",";
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < matrixPart; k++) {
+                    if (j == size - 1 && k == matrixPart - 1)
+                        fileOutput << uFull[(j * (Nt + 1) + i) * matrixPart + k] << "\n";
+                    else fileOutput << uFull[(j * (Nt + 1) + i) * matrixPart + k] << ",";
+                }
+            }
+        }
+        delete[] uFull;
+        fileOutput.close();
     }
-    fileOutput << 1 << "\n";
 
-    for (int i = 0; i <= Nt; i++) {
-      //координаты по T
-      fileOutput << i * tau << ",";
-      for (int j = 0; j < N; j++) {
-        fileOutput << uFull[i * (N + 1) + j] << ",";
-      }
-      fileOutput << uFull[i * (N + 1) + N];
-      fileOutput << "\n";
-    }
-    fileOutput.close();
-  }
-
-  //чистим память
-  MPI_Barrier(comm_1D);
-  MPI_Finalize();
-  delete[] currU;
-  delete[] uFull;
-  return 0;
+    //чистим память
+    MPI_Type_free(&MPI_RAW);
+    MPI_Comm_free(&comm_1D);
+    MPI_Finalize();
+    delete[] currU;
+    return 0;
 }
